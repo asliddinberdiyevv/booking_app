b303a6ba42c62b74dbd9f95db1b5d4c38396bef4
asliddinberdiyevv <asliddinberdiyevv@gmail.com>
2023-07-07 18:46:24 +0500

    booking cmd

--- main.go
+++ main.go
@@ -1,5 +1,63 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"strings"
+)
+
+func main() {
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	bookings := []string{}
+
+	fmt.Printf("Welcome to %s booking application\n", conferenceName)
+	fmt.Printf("We have total of %d tickets and %d are still avialable.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	for remainingTickets > 0 && len(bookings) < 50 {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		// ask user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		if userTickets <= remainingTickets {
+			remainingTickets -= userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %s %s for booking %d tickets. You will receive a confermation email at %s\n", firstName, lastName, userTickets, email)
+
+			fmt.Printf("%d tickets remaining for %s\n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				var firstName = names[0]
+				firstNames = append(firstNames, firstName)
+			}
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				// end program
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			fmt.Printf("We only have %d tickets remaining, so you can't book %d tickets\n", remainingTickets, userTickets)
+		}
+	}
 }
